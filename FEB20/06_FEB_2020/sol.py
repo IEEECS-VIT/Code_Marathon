e19d7b894a426a319c3663fb6117d6aad08c8779
Ritik <ritikgupta89369@gmail.com>
2020-02-08 06:42:31 +0530

    Today's Question Added

---
+++ FEB20/06_FEB_2020/sol.py
@@ -0,0 +1,23 @@
+#Author--Ritik Gupta
+n=int(input())
+a=[int(i) for i in input().split()]
+b=0
+ma=0
+count=0
+for i in range(n):
+    if a[i]==1:
+        b=b+1
+        count=count+1
+    elif a[i]==0 and b>0:
+        b=b-1
+        count=count+1
+    else:
+        count=0
+    if b>=ma:
+        ma=b
+        f=count
+if f==0:
+    print("1")
+else:
+    print(f)
+    
